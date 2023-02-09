781858e7ded5f4dffe1eeb421d43f4ccf2027070
natty11q <nath.fripm.san@gmail.com>
2023-02-09 23:09:06 +0000

    BIG THINGS! BIG THINGS!

--- src/main.cpp
+++ src/main.cpp
@@ -1,238 +1,264 @@
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
-#include <vector>
+//------- Ignore this ----------
+#include<filesystem>
+namespace fs = std::filesystem;
+//------------------------------
 
-#include <iostream>
-#include <vector>
-#include "global.hpp"
+#include<iostream>
+#include<glad/glad.h>
+#include<GLFW/glfw3.h>
+#include<stb/stb_image.h>
+#include<glm/glm.hpp>
+#include<glm/gtc/matrix_transform.hpp>
+#include<glm/gtc/type_ptr.hpp>
 
-#include "../header_files/shaderClass.h"
-#include "../header_files/VAO.h"
-#include "../header_files/VBO.h"
-#include "../header_files/EBO.h"
-// buffers
-/*
-*/
+#include"../header_files/Texture.h"
+#include"../header_files/shaderClass.h"
+#include"../header_files/VAO.h"
+#include"../header_files/VBO.h"
+#include"../header_files/EBO.h"
+#include"../header_files/Camera.h"
 
-// vertex shaders & fragment shaders
 
-/* 
-
-    shaders:
-
-        * draw call -> vertex shader -> fragment shader -> visual output
-
-        * vertex shader code get called for each vertex that is created
-        * vertex tell where the vertex should be rendered in the window
-        * can pass data/attributes into the next stage
-
-        * fragment shader : called once for every pixel that gets  [ rastered ]
-
-        * calculations / critical operations should be done in the vertex shader get called for each vertex
-        * however in the fragment shader it will get called for each pixel
-        * unless its things like lighting, each pixels colour value will vary so that should be valvulated in the fragment shader
-        * fragment shader, runs to calculate the colour of each pixel
-
-*/
-
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-void processInput(GLFWwindow *window);
 
 const int SCREEN_WIDTH  = 450;
 const int SCREEN_HEIGHT = 450;
 
 
-GLFWmonitor* primary_monitor = glfwGetPrimaryMonitor();
-bool fullscreen = false;
 
-std::vector<int> manageWindow(GLFWwindow *window ,int last_w, int last_h)
-{   
-    int W,H;
-    glfwGetWindowSize(window,&W,&H);
 
-    if (last_w != W and last_h != H) {
-        std::cout << "width  : " << W << std::endl;
-        std::cout << "hwight : " << H << "\n" <<std::endl;
-        glViewport(0, 0, W, H);
-    }
-    std::vector<int> return_arr = {W,H};
+// Vertices coordinates
+GLfloat vertices[] =
+{ //     COORDINATES     /        COLORS        /    TexCoord    /       NORMALS     //
+	-1.0f, 0.0f,  1.0f,		0.0f, 0.0f, 0.0f,		0.0f, 0.0f,		0.0f, 1.0f, 0.0f,
+	-1.0f, 0.0f, -1.0f,		0.0f, 0.0f, 0.0f,		0.0f, 1.0f,		0.0f, 1.0f, 0.0f,
+	 1.0f, 0.0f, -1.0f,		0.0f, 0.0f, 0.0f,		1.0f, 1.0f,		0.0f, 1.0f, 0.0f,
+	 1.0f, 0.0f,  1.0f,		0.0f, 0.0f, 0.0f,		1.0f, 0.0f,		0.0f, 1.0f, 0.0f
+};
 
-    if (W < 300){W = 300;}
-    if (H < 300){H = 300;}
-
-    return return_arr;
-}
-
-int main(int argc,const char* argv[]) 
+// Indices for vertices order
+GLuint indices[] =
 {
+	0, 1, 2,
+	0, 2, 3
+};
 
-    // inits ------------------------------------------------------- //
+GLfloat lightVertices[] =
+{ //     COORDINATES     //
+	-0.1f, -0.1f,  0.1f,
+	-0.1f, -0.1f, -0.1f,
+	 0.1f, -0.1f, -0.1f,
+	 0.1f, -0.1f,  0.1f,
+	-0.1f,  0.1f,  0.1f,
+	-0.1f,  0.1f, -0.1f,
+	 0.1f,  0.1f, -0.1f,
+	 0.1f,  0.1f,  0.1f
+};
 
-    glfwInit();                                                      // initialise glfw
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);                   // tell glfw what version of OpenGL is being used (3.3)
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);   // tell glfw that CORE profile is being used [only modern functions]
-    
-    #ifdef __APPLE__
+GLuint lightIndices[] =
+{
+	0, 1, 2,
+	0, 2, 3,
+	0, 4, 7,
+	0, 7, 3,
+	3, 7, 6,
+	3, 6, 2,
+	2, 6, 5,
+	2, 5, 1,
+	1, 5, 4,
+	1, 4, 0,
+	4, 5, 6,
+	4, 6, 7
+};
+
+
+int main()
+{
+	// Initialize GLFW
+	glfwInit();
+
+	// Tell GLFW what version of OpenGL we are using 
+	// In this case we are using OpenGL 3.3
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	// Tell GLFW we are using the CORE profile
+	// So that means we only have the modern functions
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	#ifdef __APPLE__
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     #endif
 
+	// Create a GLFWwindow object of 800 by 800 pixels, naming it "YoutubeOpenGL"
+	GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "11q combat simulator main", NULL, NULL);
+	// Error check if the window fails to create
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	// Introduce the window into the current context
+	glfwMakeContextCurrent(window);
 
-    // verticies for triangle -------------------------------------- //
-
-    float vertices[] =
-    {
-        -0.5f  , -0.5f * float(sqrt(3)) / 3  , 0.0f,   // lower left
-        0.5f  , -0.5f * float(sqrt(3)) / 3  , 0.0f,   // lower right
-        0.0f  ,  0.5f * float(sqrt(3)) * 2/3, 0.0f,  // upper corner
-        -0.5f / 2  , 0.5f * float(sqrt(3)) / 6  , 0.0f, // inner left
-        0.5f / 2  , 0.5f * float(sqrt(3)) / 6  , 0.0f, // inner right
-        0.0f  , -0.5f * float(sqrt(3)) / 3, 0.0f      // inner bottow
-    };
-    std::cout << "Created verticies" << std::endl;
-
-    GLuint indices[] = {
-        0,3,5, // L Left triangle
-        3,2,4, // L Right triangle
-        5,4,1  // U triangle
-    };
-    std::cout << "created indicies" << std::endl;
-
-    // init the window --------------------------------------------- //
-    
-    GLFWwindow* window;
-
-    if (!fullscreen){window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_WIDTH, "Enumeration praime updt", NULL, NULL);}  // create the window
-    else {window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_WIDTH, "Enumeration praime", primary_monitor, NULL);}  // <- if fullscreen
-
-    
-    if (window == NULL) {                                            // check if the window is made sucsessfully
-        std::cout << "fat fail , No window D: !" << std::endl;
-        glfwTerminate();
-        return -1;
-    }
-    glfwMakeContextCurrent(window); 
-    std::cout << "created window" << std::endl;                                 // add window to current context
-
-
-    // load glad --------------------------------------------------- //
-
-    gladLoadGL();
-
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
-        std::cout << "Failed to initialize GLAD" << std::endl;
-        return -1;
-    }
+	//Load GLAD so it configures OpenGL
+	gladLoadGL();
+	// Specify the viewport of OpenGL in the Window
+	// In this case the viewport goes from x = 0, y = 0, to x = 800, y = 800
+	glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
 
 
 
-
-    glViewport(0 ,0 , SCREEN_WIDTH, SCREEN_WIDTH); // x,y,x,y // specify veiwport
-     
-    std::cout << "generating shader objects" << std::endl;
-    // Generates Shader object using shaders defualt.vert and default.frag
+	// Generates Shader object using shaders default.vert and default.frag
 	Shader shaderProgram("/Users/natty22q/codestuffs/Cpp/games/11q_combat_simulator/recources/shaders/default.vert"
-    ,"/Users/natty22q/codestuffs/Cpp/games/11q_combat_simulator/recources/shaders/default.frag");
-
-    std::cout << "generated shader objects" << std::endl;
-
-
-    std::cout << "generate vetex arr obj" << std::endl;
+	, "/Users/natty22q/codestuffs/Cpp/games/11q_combat_simulator/recources/shaders/default.frag");
 	// Generates Vertex Array Object and binds it
 	VAO VAO1;
 	VAO1.Bind();
-    std::cout << "generated vertex arr obj" << std::endl;
-
-    std::cout << "VBO start" << std::endl;
 	// Generates Vertex Buffer Object and links it to vertices
 	VBO VBO1(vertices, sizeof(vertices));
-    std::cout << "VBO gen end" << std::endl;
-    std::cout << "EBO start" << std::endl;
 	// Generates Element Buffer Object and links it to indices
 	EBO EBO1(indices, sizeof(indices));
-    std::cout << "EBO gen end" << std::endl;
-
-    
-	// Links VBO to VAO
-	VAO1.LinkVBO(VBO1, 0);
-    std::cout << "linked VBO to VAO" << std::endl;
+	// Links VBO attributes such as coordinates and colors to VAO
+	VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 11 * sizeof(float), (void*)0);
+	VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 11 * sizeof(float), (void*)(3 * sizeof(float)));
+	VAO1.LinkAttrib(VBO1, 2, 2, GL_FLOAT, 11 * sizeof(float), (void*)(6 * sizeof(float)));
+	VAO1.LinkAttrib(VBO1, 3, 3, GL_FLOAT, 11 * sizeof(float), (void*)(8 * sizeof(float)));
 	// Unbind all to prevent accidentally modifying them
 	VAO1.Unbind();
-    std::cout << "VAO UNBINDED" << std::endl;
 	VBO1.Unbind();
-    std::cout << "VBO UNBINDED" << std::endl;
 	EBO1.Unbind();
-    std::cout << "EBO UNBINDED" << std::endl;
 
 
-    std::cout << "going to main loop" << std::endl;
-    // main game loop ------------------------------------------------ //
-    std::vector<int> sizearr = {SCREEN_WIDTH,SCREEN_HEIGHT};
-    while(!glfwWindowShouldClose(window)) {
+	// Shader for light cube
+	Shader lightShader("/Users/natty22q/codestuffs/Cpp/games/11q_combat_simulator/recources/shaders/light.vert", 
+	"/Users/natty22q/codestuffs/Cpp/games/11q_combat_simulator/recources/shaders/light.frag");
+	// Generates Vertex Array Object and binds it
+	VAO lightVAO;
+	lightVAO.Bind();
+	// Generates Vertex Buffer Object and links it to vertices
+	VBO lightVBO(lightVertices, sizeof(lightVertices));
+	// Generates Element Buffer Object and links it to indices
+	EBO lightEBO(lightIndices, sizeof(lightIndices));
+	// Links VBO attributes such as coordinates and colors to VAO
+	lightVAO.LinkAttrib(lightVBO, 0, 3, GL_FLOAT, 3 * sizeof(float), (void*)0);
+	// Unbind all to prevent accidentally modifying them
+	lightVAO.Unbind();
+	lightVBO.Unbind();
+	lightEBO.Unbind();
 
-        processInput(window);
-        sizearr = manageWindow(window,sizearr[0],sizearr[1]);
 
 
-        // specify bg colour
-        glClearColor(col_Blue[0],col_Blue[1],col_Blue[2],1.0f);   // r,g,b,a // BG colour
-        glClear(GL_COLOR_BUFFER_BIT);                             // clear the back buffer and assgn the new colour
+	glm::vec4 lightColor = glm::vec4(0.07f, 0.8f, 1.0f, 1.0f);
+	glm::vec3 lightPos = glm::vec3(0.5f, 0.5f, 0.5f);
+	glm::mat4 lightModel = glm::mat4(1.0f);
+	lightModel = glm::translate(lightModel, lightPos);
+
+	glm::vec3 objectPos = glm::vec3(0.5f, 0.5f, 0.5f);
+	glm::mat4 objectModel = glm::mat4(1.0f);
+	objectModel = glm::translate(objectModel, objectPos);
 
 
-        // Tell OpenGL which Shader Program we want to use
+	lightShader.Activate();
+	glUniformMatrix4fv(glGetUniformLocation(lightShader.ID, "model"), 1, GL_FALSE, glm::value_ptr(lightModel));
+	glUniform4f(glGetUniformLocation(lightShader.ID, "lightColor"), lightColor.x, lightColor.y, lightColor.z, lightColor.w);
+	shaderProgram.Activate();
+	glUniformMatrix4fv(glGetUniformLocation(shaderProgram.ID, "model"), 1, GL_FALSE, glm::value_ptr(objectModel));
+	glUniform4f(glGetUniformLocation(shaderProgram.ID, "lightColor"), lightColor.x, lightColor.y, lightColor.z, lightColor.w);
+	glUniform3f(glGetUniformLocation(shaderProgram.ID, "lightPos"), lightPos.x, lightPos.y, lightPos.z);
+
+
+
+	/*
+	* I'm doing this relative path thing in order to centralize all the resources into one folder and not
+	* duplicate them between tutorial folders. You can just copy paste the resources from the 'Resources'
+	* folder and then give a relative path from this folder to whatever resource you want to get to.
+	* Also note that this requires C++17, so go to Project Properties, C/C++, Language, and select C++17
+	*/
+	std::string parentDir = (fs::current_path().fs::path::parent_path()).string();
+	std::string texPath = "/Users/natty22q/codestuffs/Cpp/games/11q_combat_simulator/recources/textures/";
+
+	// Textures
+	Texture planksTex((texPath + "planks.png").c_str(), GL_TEXTURE_2D, 0, GL_RGBA, GL_UNSIGNED_BYTE);
+	planksTex.texUnit(shaderProgram, "tex0", 0);
+	Texture planksSpec((texPath + "planksSpec.png").c_str(), GL_TEXTURE_2D, 1, GL_RED, GL_UNSIGNED_BYTE);
+	planksSpec.texUnit(shaderProgram, "tex1", 1);
+
+	// Original code from the tutorial
+	/*Texture planksTex("planks.png", GL_TEXTURE_2D, 0, GL_RGBA, GL_UNSIGNED_BYTE);
+	planksTex.texUnit(shaderProgram, "tex0", 0);
+	Texture planksSpec("planksSpec.png", GL_TEXTURE_2D, 1, GL_RED, GL_UNSIGNED_BYTE);
+	planksSpec.texUnit(shaderProgram, "tex1", 1);*/
+
+
+
+	// Enables the Depth Buffer
+	glEnable(GL_DEPTH_TEST);
+
+	// Creates camera object
+	Camera camera(SCREEN_WIDTH, SCREEN_HEIGHT, glm::vec3(0.0f, 0.0f, 2.0f));
+
+	// Main while loop
+	while (!glfwWindowShouldClose(window))
+	{
+		// Specify the color of the background
+		glClearColor(0.01f, 0.01f, 0.01f, 1.0f);
+		// Clean the back buffer and depth buffer
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		// Handles camera inputs
+		camera.Inputs(window);
+		// Updates and exports the camera matrix to the Vertex Shader
+		camera.updateMatrix(45.0f, 0.1f, 100.0f);
+
+
+		// Tells OpenGL which Shader Program we want to use
 		shaderProgram.Activate();
+		// Exports the camera Position to the Fragment Shader for specular lighting
+		glUniform3f(glGetUniformLocation(shaderProgram.ID, "camPos"), camera.Position.x, camera.Position.y, camera.Position.z);
+		// Export the camMatrix to the Vertex Shader of the pyramid
+		camera.Matrix(shaderProgram, "camMatrix");
+		// Binds textures so that they appear in the rendering
+		planksTex.Bind();
+		planksSpec.Bind();
 		// Bind the VAO so OpenGL knows to use it
 		VAO1.Bind();
 		// Draw primitives, number of indices, datatype of indices, index of indices
+		glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(int), GL_UNSIGNED_INT, 0);
 
-        glDrawElements(GL_TRIANGLES,9,GL_UNSIGNED_INT ,0);
-        // glBindVertexArray(0);
 
-        glfwSwapBuffers(window);
 
-        glfwPollEvents(); // handles main GLFW events
+		// Tells OpenGL which Shader Program we want to use
+		lightShader.Activate();
+		// Export the camMatrix to the Vertex Shader of the light cube
+		camera.Matrix(lightShader, "camMatrix");
+		// Bind the VAO so OpenGL knows to use it
+		lightVAO.Bind();
+		// Draw primitives, number of indices, datatype of indices, index of indices
+		glDrawElements(GL_TRIANGLES, sizeof(lightIndices) / sizeof(int), GL_UNSIGNED_INT, 0);
 
-    }
-    
-    std::cout<< "ending" <<std::endl;
-    VAO1.Delete();
+
+		// Swap the back buffer with the front buffer
+		glfwSwapBuffers(window);
+		// Take care of all GLFW events
+		glfwPollEvents();
+	}
+
+
+
+	// Delete all the objects we've created
+	VAO1.Delete();
 	VBO1.Delete();
 	EBO1.Delete();
+	planksTex.Delete();
+	planksSpec.Delete();
 	shaderProgram.Delete();
-
-    glfwDestroyWindow(window);
-
-    glfwTerminate();
-
-    // std::cout<< "press enter to quit" <<std::endl;
-    // std::cin.get();
-
-    return 0;
-}
-
-
-// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
-// ---------------------------------------------------------------------------------------------------------
-void processInput(GLFWwindow *window)
-{
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS){
-        glfwSetWindowShouldClose(window, true);}
-    if (glfwGetKey(window, GLFW_KEY_3) == GLFW_PRESS){
-        glfwSetWindowSize(window,300,300);}
-}
-
-
-// glfw: whenever the window size changed (by OS or user resize) this callback function executes
-// ---------------------------------------------------------------------------------------------
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-{
-    // make sure the viewport matches the new window dimensions; note that width and 
-    // height will be significantly larger than specified on retina displays.
-    if (width < 300){width = 300;}
-    if (height < 300){height = 300;}
-
-    std::cout << "width  : " << width << std::endl;
-    std::cout << "hwight : " << height << std::endl;
-    glViewport(0, 0, width, height);
+	lightVAO.Delete();
+	lightVBO.Delete();
+	lightEBO.Delete();
+	lightShader.Delete();
+	// Delete window before ending the program
+	glfwDestroyWindow(window);
+	// Terminate GLFW before ending the program
+	glfwTerminate();
+	return 0;
 }
